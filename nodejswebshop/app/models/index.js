af9dd18bc6b7af5a298065c4ccc73a1244489348
samumuku <163824157+samumuku@users.noreply.github.com>
2025-01-13 15:42:03 +0100

    feat(nodejs): Installation de Express
    J'ai installé Express puis modifié un peu le code en JS

---
+++ nodejswebshop/app/models/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>My Website</title>
+        </head>
+        <body>
+            <h1>Welcome to My Website</h1>
+            <p>This is a simple HTML page served using Express.</p>
+        </body>
+        </html>
+    `);
+});
